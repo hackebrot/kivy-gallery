8ea85b9785ad0bbaa2ba7ea5fef2bd06cc7c2b68
Raphael Pierzina <raphael@hackebrot.de>
2014-02-12 22:58:30 +0100

    Create basic app

---
+++ main.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python2.7
+# -*- coding: utf-8 -*-
+
+from tour.tourapp import TourApp
+
+
+if __name__ in ('__main__', '__android__'):
+    TourApp().run()
+
---
+++ tour/tourapp.py
@@ -0,0 +1,6 @@
+from kivy.app import App
+
+
+class TourApp(App):
+    def build(self):
+        return self.root
