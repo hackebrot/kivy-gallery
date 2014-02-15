c129c1795234312601250f27b9ec862cdbbef1c4
Raphael Pierzina <raphael@hackebrot.de>
2014-02-15 21:13:40 +0100

    Create SideBarMenu module

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ kivy.require('1.8.0')
 
 from tour.tourapp import TourApp
 from tour.sidebarlabel import SideBarLabel
+from tour.sidebarmenu import SideBarMenu
 
 
 if __name__ in ('__main__', '__android__'):
---
+++ tour/sidebarmenu.py
@@ -0,0 +1,7 @@
+from kivy.uix.boxlayout import BoxLayout
+
+
+class SideBarMenu(BoxLayout):
+    """Menu used in every screen for global actions.
+    """
+    pass
