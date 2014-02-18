dd3d8f2be4afb93196af06af19dabf97201331bc
Raphael Pierzina <raphael@hackebrot.de>
2014-02-18 21:06:34 +0100

    Implement basic config builder

--- tour/tourapp.py
+++ tour/tourapp.py
@@ -1,7 +1,15 @@
+from os import path
 from kivy.app import App
 from tour.screens import ScreenMgr
 
 
 class TourApp(App):
+    def build_config(self, config):
+        config.adddefaultsection('media')
+        config.setdefault('media', 'dir', 'media')
+        config.setdefault('media', 'playlist', 'playlist.json')
+
     def build(self):
+        self.mediaDir = self.config.get('media', 'dir')
+        self.playlist = self.config.get('media', 'playlist')
         return ScreenMgr()
