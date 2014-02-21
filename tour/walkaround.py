70a2d0415137d42d79e6628cea915ca16a6ca5bf
Raphael Pierzina <raphael@hackebrot.de>
2014-02-22 00:21:28 +0100

    Implement walkaround widget

---
+++ tour/walkaround.py
@@ -0,0 +1,37 @@
+from os import path
+from glob import glob
+
+from kivy.uix.image import AsyncImage
+from kivy.uix.floatlayout import FloatLayout
+
+
+class Walkaround(FloatLayout):
+    """Widget providing intuitive controls for exploring
+    a sequence of lazy loaded images.
+    """
+    current = 0
+
+    def __init__(self, imgDir):
+        super(Walkaround, self).__init__()
+        self.loadImages(path.join(imgDir, '*'))
+        self.updateImage(0)
+
+    def loadImages(self, imgDir):
+        """Create an AsyncImage instance for each file in the given folder.
+        """
+        fNames = sorted(glob(imgDir))
+        self.images = list(AsyncImage(source=fn) for fn in fNames)
+
+    def updateImage(self, idx):
+        """Display the image at index and store the index.
+        """
+        self.clear_widgets()
+        self.add_widget(self.images[idx])
+        self.current = idx
+
+    def on_touch_move(self, touch):
+        """Change the image on user interaction based on the movements speed.
+        """
+        step = int(touch.dx / 5)
+        self.updateImage((self.current - step) % len(self.images))
+
