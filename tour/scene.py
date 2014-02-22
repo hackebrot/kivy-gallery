d111ae884db41d47499305ca1fb50ba536cf8f97
Raphael Pierzina <raphael@hackebrot.de>
2014-02-22 17:51:19 +0100

    Implement scene widget

---
+++ tour/scene.py
@@ -0,0 +1,19 @@
+from kivy.uix.video import Video
+
+
+class Scene(Video):
+    """Custom Video showing the given file.
+    """
+    def __init__(self, videoFile):
+        super(Scene, self).__init__(source=videoFile)
+
+    def toggleState(self):
+        self.state = 'pause' if self.state == 'play' else 'play'
+
+    def on_touch_down(self, touch):
+        """Simply toggle between pause and play.
+        """
+        if self.collide_point(*touch.pos):
+            self.toggleState()
+            return True
+
