36a1f7c755e0b0b35c96c3171f60fca1c6a7dbd6
Raphael Pierzina <raphael@hackebrot.de>
2014-09-14 17:24:05 +0200

    Use smaller resolution

--- tour/tourapp.py
+++ tour/tourapp.py
@@ -1,8 +1,8 @@
 import kivy
 kivy.require('1.8.0')
 from kivy.config import Config
-Config.set('graphics', 'width', 1920)
-Config.set('graphics', 'height', 1080)
+Config.set('graphics', 'width', 960)
+Config.set('graphics', 'height', 540)
 #Use this to set the size of the app as suggested at:
 #https://groups.google.com/forum/#!topic/kivy-users/TR7UycgcLpQ
 
